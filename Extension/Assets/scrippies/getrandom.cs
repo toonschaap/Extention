cba6c6bd772de53207a389ec286ec810b362937b
Toon <toonschaap3@gmail.com>
2020-01-10 11:40:04 +0100

    ewf

---
+++ Extension/Assets/scrippies/getrandom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getrandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> words = new List<string>();
+        words.Add("stinky");
+        words.Add("poo poo");
+        words.Add("uh oh");
+        words.Add("funny poo poo");
+
+        Debug.Log(words.GetRandomValue());
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Extension/Assets/scrippies/sety.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sety : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetPositionY(3);
+        Debug.Log("thingie moved a bit");
+        
+    }
+}
